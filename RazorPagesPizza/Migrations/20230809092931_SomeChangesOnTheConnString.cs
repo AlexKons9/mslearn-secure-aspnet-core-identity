c641621aec9a40dcb45124d64fc3a09a2c93eb5c
AlexKons9 <alex.lepeniotis@gmail.com>
2023-08-10 17:25:17 +0300

    First & Last name and changed verification method

---
+++ RazorPagesPizza/Migrations/20230809092931_SomeChangesOnTheConnString.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RazorPagesPizza.Migrations
+{
+    public partial class SomeChangesOnTheConnString : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- RazorPagesPizza/Program.cs
+++ RazorPagesPizza/Program.cs
@@ -1,7 +1,23 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesPizza.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using RazorPagesPizza.Services;
 
+var builder = WebApplication.CreateBuilder(args);
+// The EF Core database context class, named RazorPagesPizzaAuth, is configured with the connection string.
+var connectionString = builder.Configuration.GetConnectionString("RazorPagesPizzaAuthConnection");
+builder.Services.AddDbContext<RazorPagesPizzaAuth>(options => options.UseSqlServer(connectionString));
+// AddDefaultIdentity<IdentityUser> tells the Identity services to use the default user model.
+// The lambda expression options => options.SignIn.RequireConfirmedAccount = true 
+// specifies that users must confirm their email accounts.
+// .AddEntityFrameworkStores<RazorPagesPizzaAuth>() specifies that Identity uses the default 
+// Entity Framework Core store for its database. The RazorPagesPizzaAuth DbContext class is used.
+builder.Services.AddDefaultIdentity<RazorPagesPizzaUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<RazorPagesPizzaAuth>();
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddTransient<IEmailSender, EmailSender>();
 
 var app = builder.Build();
 
@@ -17,7 +33,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapRazorPages();
